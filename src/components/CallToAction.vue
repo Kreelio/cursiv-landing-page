b79f1efa6becb4b579636c69f69f954214694805
guiadco <guiadco@geekhomeinside.com>
2021-08-02 20:48:01 +0200

    chore(): test section

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -4,7 +4,7 @@
         Testez dès maintenant!
       </h1>
       <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-        S'inscrire!
+        Cliquez-ici !
       </button>
     </section>
 </template>
